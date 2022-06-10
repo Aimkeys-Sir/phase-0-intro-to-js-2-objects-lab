72e74eaa8c662e32d7fe7de3801786a91914a393
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-10 10:15:17 +0300

    commit tree d1ad12e94cfa59afa995866e551e4e01e81729ed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee={name:"Sam", streetAddress:"Gatura"}
+
+function updateEmployeeWithKeyAndValue(employee,name="name",value="Sam")
+{
+    const employees_updated={...employee};
+    employees_updated[name]=value;
+     return employees_updated;
+}
+//console.log(updateEmployeeWithKeyAndValue(employees,"name","Guy"));
+
+function deleteFromEmployeeByKey(employees=employee, key="name")
+{
+    const new_employees= {...employees};
+    delete new_employees.name;
+    return new_employees;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employees=employee, key="name", value="Sam")
+{
+employees[key]=value;
+return employees;
+}
+function destructivelyDeleteFromEmployeeByKey(employees=employee, key="name")
+{
+delete employee.name;
+return employee;
+}
